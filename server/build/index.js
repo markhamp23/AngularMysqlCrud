3da68cc4c1b5bdaf408db5b2088d7bd677b418a7
patrick <pamarkham@hotmail.com>
2019-03-04 13:30:52 +0100

    Continue generate server

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('server');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+    }
+    config() {
+    }
+    routes() {
+    }
+    start() {
+    }
+}
+new Server();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('server')
+import express, { Application } from 'express';
+
+class Server {
+
+    app: Application;
+
+    constructor(){
+        this.app = express();
+    }
+
+    config(): void {
+    }
+
+    routes(): void{
+    }
+
+    start(): void{
+    }
+}
+
+new Server();
